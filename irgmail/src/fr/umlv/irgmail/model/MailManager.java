e45b1f61d141f6b527fe2ae62da8b83b158b51da
Derfic <Derfic77@gmail.com>
2015-11-26 17:14:15 +0100

    Un peu de doc

--- irgmail/src/fr/umlv/irgmail/model/MailManager.java
+++ irgmail/src/fr/umlv/irgmail/model/MailManager.java
@@ -13,16 +13,37 @@ import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 
+/**
+ * Implementation of a mail manager which can convert Messages of a Folder to a JSON formatable Object.
+ * This class supports concurrent modifications.
+ * @see Folder
+ * @see Message
+ *
+ */
 public class MailManager {
 
 	private static final int PAGE_OFFSET = 10;
 
+	/**
+	 * The folder used to get the Messages from.
+	 */
 	private Folder inbox;
+	
+	/**
+	 * A local copy of the number of Messages in the folder.
+	 */
 	private int mailsCounter;
+	
+	/**
+	 * A Thread that will allows to get the updates of the distant folder
+	 */
 	private final Thread updater;
 	private ConcurrentHashMap<Integer, Header> headers;
 	private ConcurrentHashMap<Integer, Content> contents;
 
+	/**
+	 * Constructs a manager with the default parameters initialized.
+	 */
 	public MailManager() {
 		mailsCounter = 0;
 		updater = new Thread(() -> {
@@ -80,6 +101,12 @@ public class MailManager {
 		}
 	}
 
+	/**
+	 * Returns a Stream of JSON formated mail.
+	 * @param keywords a String array that contains the key words of the search.
+	 * @return Stream<String> a Stream of JSON formated mail.
+	 * @throws MessagingException if the distant request on Folder didn't work.
+	 */
 	public Stream<String> headersByKeywords(String... keywords)
 			throws MessagingException {
 		collectHeadersByKeyword(keywords);
@@ -92,6 +119,12 @@ public class MailManager {
 		return list.stream().map(Header::toJSONString);
 	}
 
+	/**
+	 * Returns a Stream of JSON formated mails.
+	 * @param page an int representing the page in the current Folder.
+	 * @return Stream<String> a Stream of JSON formated mail.
+	 * @throws MessagingException if the distant request on Folder didn't work.
+	 */
 	public Stream<String> headersByPage(int page) throws MessagingException {
 		ArrayList<Header> tmp = new ArrayList<Header>();
 		int start, end = 0;
@@ -106,6 +139,13 @@ public class MailManager {
 		return tmp.stream().map(Header::toJSONString);
 	}
 
+	/**
+	 * Returns a single mail to a JSON format.
+	 * @param index the index of the mail in the current Folder.
+	 * @return String a mail in a JSON format.
+	 * @throws MessagingException if the distant request on Folder didn't work.
+	 * @throws IOException if the attachments collecting failed.
+	 */
 	public String mailToJSON(int index) throws MessagingException, IOException {
 		if (contents.get(index) == null) {
 			collectContent(index);
@@ -114,6 +154,12 @@ public class MailManager {
 				+ contents.get(index).toJSONString().replace("{", "");
 	}
 
+	/**
+	 * Starts the mail managing on a given Folder.
+	 * This method should be called first after the constructor.
+	 * @param folder a Folder to be opened and to retrieve from.
+	 * @throws MessagingException if the folder opening failed.
+	 */
 	public void startOnFolder(Folder folder) throws MessagingException {
 		inbox = Objects.requireNonNull(folder);
 		inbox.open(READ_WRITE);
